5d6a84631f71c815d85966f1f8f27ca7b5c0bea3
Viktor Kurysh <vik8174@gmail.com>
2022-10-30 19:24:39 +0200

    feat: navigate with dynamic routes

--- pages/clients/index.tsx
+++ pages/clients/index.tsx
@@ -1,7 +1,18 @@
+import Link from 'next/link';
+
 export default function ClientsPage() {
   return (
     <div>
       <h1>The Clients page</h1>
+
+      <ul>
+        <li>
+          <Link href="/clients/viktor-kurysh">Viktor Kurysh</Link>
+        </li>
+        <li>
+          <Link href="/clients/john-doe">John Doe</Link>
+        </li>
+      </ul>
     </div>
   );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,4 @@
-import Link from 'next/link'
+import Link from 'next/link';
 
 export default function HomePage() {
   return (
@@ -19,5 +19,5 @@ export default function HomePage() {
         </li>
       </ul>
     </div>
-  )
+  );
 }
