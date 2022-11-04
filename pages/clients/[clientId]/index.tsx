05d9a76248abba67dbf64946235b300635247180
Viktor Kurysh <vik8174@gmail.com>
2022-11-04 13:56:45 +0200

    feat: navigate programmatically

--- pages/clients/[clientId]/index.tsx
+++ pages/clients/[clientId]/index.tsx
@@ -1,7 +1,16 @@
+import { useRouter } from 'next/router';
+
 export default function ClientProjectPage() {
+  const router = useRouter();
+
+  const onLoadProject = () => {
+    router.push('/clients/jane-doe/demo-project');
+  };
+
   return (
     <div>
       <h1>The Client&apos;s Info Page</h1>
+      <button onClick={onLoadProject}>Load Project</button>
     </div>
   );
 }
